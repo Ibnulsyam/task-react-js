9b20d9178d59a727b47c70637d143ff56e48af18
ibnul syam <ibnulsyam6@gmail.com>
2022-05-17 15:29:28 +0800

    update Exprerience.jsx

--- src/Tugas/Routing/Component/Experience/Experience.jsx
+++ src/Tugas/Routing/Component/Experience/Experience.jsx
@@ -17,7 +17,10 @@ const Experience = () => {
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat nobis
         est odit nesciunt maiores quia adipisci. Unde, dolores veritatis rerum
         porro veniam esse nemo repellendus, cumque culpa dignissimos similique
-        voluptatum.
+        voluptatum. Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+        Blanditiis odit praesentium minus libero, beatae repellendus autem iusto
+        veniam deserunt corporis molestias, deleniti ratione vero tempore harum
+        ex commodi aliquam quibusdam?
       </p>
     </ConExperience>
   );
